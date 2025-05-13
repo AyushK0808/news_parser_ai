b0d1d5f1e6733ae5b6465a68a1a561157b3251b1
Ayush Kumar <theofficialayush.kumar@gmail.com>
2025-05-13 19:58:39 +0530

    fix:deployment pt2

--- api/index.py
+++ api/index.py
@@ -2,7 +2,9 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app as handler
+from app import app
+
+handler=app
 
 if __name__ == "__main__":
     handler.run(debug=True)
